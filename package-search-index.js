f35dd69de50c31a29c1563ad79b6f19bf51f2db4
davio-viator <47100139+davio-viator@users.noreply.github.com>
2021-04-07 21:33:28 +0200

    Delete package-search-index.js

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"tamagohis.game","l":"tamagoshis"},{"m":"tamagohis.game","l":"tamagoshis.graphics"}];updateSearchResults();
